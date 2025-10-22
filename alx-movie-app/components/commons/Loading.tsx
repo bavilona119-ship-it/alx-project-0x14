5adc86fff8afad902a418a3d073907d297cbb8b5
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-22 19:48:04 +0300

    Create Loading.tsx

---
+++ alx-movie-app/components/commons/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center py-10">
+      <p className="text-gray-500 animate-pulse">Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
