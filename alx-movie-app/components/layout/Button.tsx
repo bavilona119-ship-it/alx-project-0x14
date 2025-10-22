df3b81c1c223517047e4d58caea45c1db15e1e06
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-22 19:50:15 +0300

    Create Button.tsx

---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">
+      Layout Button
+    </button>
+  );
+};
+
+export default Button;
