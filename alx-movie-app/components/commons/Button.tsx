4255e2876fa52ac2fa0e44babb1bf5345486feff
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-22 19:37:15 +0300

    Create Button.tsx

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+      Click Me
+    </button>
+  );
+};
+
+export default Button;
